1cbff3157da52a815bfef913fd9d0308e079cd5a
Atti Ur Rehman <atti.rehmman@gmail.com>
2024-02-11 14:07:11 +0200

    fixes the bug in CarController

--- AynaCar/AynaCar/Controllers/CarController.cs
+++ AynaCar/AynaCar/Controllers/CarController.cs
@@ -15,27 +15,4 @@ public class CarContoller : ControllerBase
         return Ok(request);
     }
 
-    // Endpoint to get a car by its ID
-    [HttpGet("/cars/{id: guid}")]
-
-    public IActionResult GetCar(Guid id)
-    {
-        return Ok(id);
-    }
-
-    // Endpoint to update or insert a car
-    [HttpPut("/cars/{id: guid}")]
-
-    public IActionResult UpsertCar(Guid id, UpsertCarRequest request)
-    {
-        return Ok(request);
-    }
-
-    // Endpoint to delete a car by its ID
-    [HttpDelete("/cars/{id: guid}")]
-
-    public IActionResult DeleteCar(Guid id)
-    {
-        return Ok(id);
-    }
 }
