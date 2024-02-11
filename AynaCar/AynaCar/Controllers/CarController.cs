3dd5bb44d9487cf4774ed99447611c38c46fc86c
Atti Ur Rehman <atti.rehmman@gmail.com>
2024-02-11 14:57:23 +0200

    Adds Get put and delete endpoints and their corresponding Rest client requests

--- AynaCar/AynaCar/Controllers/CarController.cs
+++ AynaCar/AynaCar/Controllers/CarController.cs
@@ -15,4 +15,24 @@ public class CarContoller : ControllerBase
         return Ok(request);
     }
 
+    // Endpoint to get a Car by id
+    [HttpGet("/cars/{id:guid}")]
+    public IActionResult GetCar(Guid id)
+    {
+        return Ok(id);
+    }
+
+    // Endpoint to put a Car
+    [HttpPut("{id:guid}")]
+    public IActionResult UpsertBreakfast(Guid id, UpsertCarRequest request)
+    {
+        return Ok(request);
+    }
+
+    // Endpoint to Delte a Car by id
+    [HttpDelete("/cars/{id:guid}")]
+    public IActionResult DeleteCar(Guid id)
+    {
+        return Ok(id);
+    }
 }
